b5341402eb869a96ceb392a33b57cef5f85c077b
Jimbo587 <70841882+Jimbo587@users.noreply.github.com>
2020-10-04 13:00:28 +0200

    changing question structure

--- app/src/main/java/com/example/madlevel2task2/Question.kt
+++ app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -1,47 +1,21 @@
 package com.example.madlevel2task2
 
-import androidx.annotation.DrawableRes
-
 data class Question(
-    var questionText: String,
-    var answer: String
+    val questionText: String,
+    val answer: Boolean
 ) {
     companion object {
-        val QUESTIONS = arrayOf(
-            "No number from 1 to 999 includes the letter \"a\" in its word form.",
-            "The opposite sides of a die will always add up to eight.",
-            "Playing dance music can't help ward off mosquitoes.",
-            "Cats can't taste sweet things because of a genetic defect.",
-            "It's possible to turn peanut butter into diamonds.",
-            "The average adult spends more time on the toilet than they do exercising.",
-            "Your dominant hand grows faster then your fingernails.",
-            "A \"jiffy\" is about one trillionth of a second.",
-            "Dragonflies have four legs but can't walk.",
-            "Watermelon seeds contain cyanide."
-        )
-        val QUESTIONS_ANWSER = arrayOf(
-            /*true,
-            false,
-            false,
-            true,
-            true,
-            true,
-            false,
-            true,
-            false,
-            false
-            */
-                "True",
-                "False",
-                "False",
-                "True",
-                "True",
-                "True",
-                "False",
-                "True",
-                "False",
-                "False",
-
+        val QUESTIONS = listOf(
+            Question("No number from 1 to 999 includes the letter \"a\" in its word form.", true),
+            Question("The opposite sides of a die will always add up to eight.", false),
+            Question("Playing dance music can't help ward off mosquitoes.", false),
+            Question("Cats can't taste sweet things because of a genetic defect.", true),
+            Question("It's possible to turn peanut butter into diamonds.", true),
+            Question("The average adult spends more time on the toilet than they do exercising.", true),
+            Question("Your dominant hand grows faster then your fingernails.", false),
+            Question("A \"jiffy\" is about one trillionth of a second.", true),
+            Question("Dragonflies have four legs but can't walk.", false),
+            Question("Watermelon seeds contain cyanide.", false)
         )
     }
 }
