8d124c1f525b2f52e785427fadbf51b5f0a9e643
Jimbo587 <70841882+Jimbo587@users.noreply.github.com>
2020-10-04 14:30:19 +0200

    adding the onSwipe function and finishing the app

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,9 +29,45 @@ class MainActivity : AppCompatActivity() {
             LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         binding.rvQuestions.adapter = questionAdapter
         for (i in Question.QUESTIONS.indices) {
-            questions.add(Question(Question.QUESTIONS[i], Question.QUESTIONS_ANWSER[i]))
-
+            questions.add(Question(Question.QUESTIONS[i].questionText, Question.QUESTIONS[i].answer))
         }
+
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        createItemTouchHelper().attachToRecyclerView(rv_questions)
         questionAdapter.notifyDataSetChanged()
     }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                if (questions[position].answer == direction) {
+                    questions.removeAt(position)
+                } else {
+                    Snackbar.make(
+                        binding.rvQuestions[position],
+                        "Incorrect answer, try again!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+                questionAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+
 }
--- app/src/main/java/com/example/madlevel2task2/Question.kt
+++ app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -1,21 +1,25 @@
 package com.example.madlevel2task2
 
+import androidx.recyclerview.widget.ItemTouchHelper
+
 data class Question(
     val questionText: String,
-    val answer: Boolean
+    val answer: Int
 ) {
     companion object {
-        val QUESTIONS = listOf(
-            Question("No number from 1 to 999 includes the letter \"a\" in its word form.", true),
-            Question("The opposite sides of a die will always add up to eight.", false),
-            Question("Playing dance music can't help ward off mosquitoes.", false),
-            Question("Cats can't taste sweet things because of a genetic defect.", true),
-            Question("It's possible to turn peanut butter into diamonds.", true),
-            Question("The average adult spends more time on the toilet than they do exercising.", true),
-            Question("Your dominant hand grows faster then your fingernails.", false),
-            Question("A \"jiffy\" is about one trillionth of a second.", true),
-            Question("Dragonflies have four legs but can't walk.", false),
-            Question("Watermelon seeds contain cyanide.", false)
+        private val correct = ItemTouchHelper.LEFT
+        private val incorrect = ItemTouchHelper.RIGHT
+        val QUESTIONS : Array<Question> = arrayOf(
+            Question( "No number from 1 to 999 includes the letter \"a\" in its word form.", correct),
+            Question( "The opposite sides of a die will always add up to eight.", incorrect),
+            Question( "Playing dance music can't help ward off mosquitoes.", incorrect),
+            Question( "Cats can't taste sweet things because of a genetic defect.", correct),
+            Question( "It's possible to turn peanut butter into diamonds.", correct),
+            Question( "The average adult spends more time on the toilet than they do exercising.", correct),
+            Question( "Your dominant hand grows faster then your fingernails.", incorrect),
+            Question( "A \"jiffy\" is about one trillionth of a second.", correct),
+            Question( "Dragonflies have four legs but can't walk.", incorrect),
+            Question( "Watermelon seeds contain cyanide.", incorrect)
         )
     }
 }
